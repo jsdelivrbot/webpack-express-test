b0c65745e7a5dfd6d120fa528a494cf362148b65
Suhyun <searchforac@gmail.com>
2017-03-26 17:28:35 -0400

    adds .env .gitignore components

--- server/index.js
+++ server/index.js
@@ -1,17 +1,15 @@
 var express = require('express')
 var path = require('path')
-var http = require('http')
 var port = process.env.PORT || 3090
 var app = express()
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../dist')))
-console.log(path.resolve(__dirname, '../dist'))
+
 app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, '../index.html'))
 })
 
 // Server Setup
-var server = http.createServer(app)
-server.listen(port)
+app.listen(port)
 console.log('Server listening on: ' + port)
