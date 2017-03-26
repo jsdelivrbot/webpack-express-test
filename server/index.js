d7efa6610b2277ac8804513e0d5b8c105a65b6a7
Suhyun <searchforac@gmail.com>
2017-03-26 16:08:40 -0400

    moves start before postinstall

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var path = require('path')
+var port = process.env.PORT || 3090
 var app = express()
 
 app.use(express.static(path.join(__dirname, '../dist')))
@@ -7,3 +8,6 @@ app.use(express.static(path.join(__dirname, '../dist')))
 app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, './index.html'))
 })
+
+app.listen(port)
+console.log('server started on port: ' + port)
