5a42ce22f85e4bcdd3242ceb76989a8f271be15c
Suhyun <searchforac@gmail.com>
2017-03-26 16:11:49 -0400

    removes / in get

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ var app = express()
 app.use(express.static(path.join(__dirname, '../dist')))
 
 app.get('*', function (req, res) {
-  res.sendFile(path.resolve(__dirname, './index.html'))
+  res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
 app.listen(port)
