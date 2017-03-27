5b4fcbd820315e99b0c7b867a1f7cca664af3d6e
Suhyun <searchforac@gmail.com>
2017-03-27 09:04:58 -0400

    reinstall babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   module: {
     rules: [
       {
-        loader: "babel-loader",
+        loader: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
       }
