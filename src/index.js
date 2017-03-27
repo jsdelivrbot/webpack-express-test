0d29fad1a5693a2cd16fc2513418641e3b1690b7
Suhyun <searchforac@gmail.com>
2017-03-26 22:08:35 -0400

    adds react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-document.write(require('./content.js'))
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return <h1>Hello</h1>
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
