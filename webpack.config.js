33d354815a5dd7be5cfd4c2a8d179968600c73f8
Suhyun <searchforac@gmail.com>
2017-03-26 22:33:40 -0400

    changes babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,10 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
     ]
   }
 }
