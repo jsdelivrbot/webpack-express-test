2a06997fc1b02b76a23e40e6a13abc5f57bf2f51
Suhyun <searchforac@gmail.com>
2017-03-26 23:22:00 -0400

    change loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   module: {
     rules: [
       {
-        use: 'babel-loader',
+        loader: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
       }
